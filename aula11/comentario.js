e26b4aa060052014f1ba72bb26c4559e3d8b48b9
GabrielyMota <gabrielymota26@gmail.com>
2021-12-07 13:37:42 -0400

    até aula 13

---
+++ aula11/comentario.js
@@ -0,0 +1,12 @@
+//Escrever um comentário
+console.log('Olá mundo!'); //Aqui outro comentário
+console.log('Linha 1');
+//Comentário
+
+/* 
+* Um título 
+*/
+console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+//Vai ser ignorado
