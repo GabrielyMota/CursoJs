de2eb31f452d81b17ff519ce0142466301ab3ecc
GabrielyMota <gabrielymota26@gmail.com>
2021-12-16 09:22:10 -0400

    mudanças

---
+++ aula17/index.js
@@ -0,0 +1,11 @@
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const conta = primeiroNumero * segundoNumero; 
+let resultadoTriplicado = resultado * 3;
+console.log(resultado);
+
+/*********/
+
+const nome = 'João';
+console.log(nome);
